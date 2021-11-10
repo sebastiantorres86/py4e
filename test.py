60ba17000984e330357afc517fb57df13b45ea16
sebastiantorres86 <seba.torres.dev@gmail.com>
2021-11-10 19:41:50 -0300

    ex_05_01

---
+++ test.py
@@ -0,0 +1,17 @@
+fruit = 'banana'
+# index = 0
+# while index < len(fruit):
+#     letter = fruit[index]
+#     print(index, letter)
+#     index = index + 1
+
+# for letter in fruit:
+#     print(letter)
+
+# word = 'banana'
+# count = 0
+# for letter in word:
+#     if letter == 'a':
+#         count = count + 1
+# print(count)
+
